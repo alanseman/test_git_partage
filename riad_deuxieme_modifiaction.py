57732d31d26b0b0ece6c33579453330c1b6b41fb
unknown <riadaerty@gmail.com>
2023-02-16 15:29:00 +0100

    ma deuxieme modi

---
+++ riad_deuxieme_modifiaction.py
@@ -0,0 +1,14 @@
+print("Bonjour tout le monde")
+print("salut !")
+
+print("why not")
+print("hhhh")
+
+print("happy days")
+
+print("happy happy days")
+
+print("hay !")
+
+
+print(" j' ai ajouer une branche riad")
